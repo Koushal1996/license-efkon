a28f90087720fcc5fc5e27e3037c89be3bd90783
Jamshed332 <jmd.amd144@gmail.com>
2020-03-03 19:04:02 +0530

    Change in UserDetail,service,login,users

--- src/app/main/project-detail/project-detail.component.ts
+++ src/app/main/project-detail/project-detail.component.ts
@@ -9,12 +9,15 @@ import { ProjectDetailServiceService } from './project-detail-service.service';
 export class ProjectDetailComponent implements OnInit {
 myProjectDetail:any=[];
 projectStatus:boolean
-
+Searchvalue:number;
+SortValue:number=1;
+msg:string
   constructor(private _myServices:ProjectDetailServiceService) { }
   alerts:boolean=false
   ngOnInit(): void {
     this.myProjectDetail=this._myServices.projectDetail
   }
+  // Delete Project Condition
   DeleteProject(i)
   {
    var date1 = new Date();
@@ -34,9 +37,70 @@ if(val)
 
 
 }
-  }
+}
+
   OnProjectSubmit(i)
   {
 this.projectStatus=!this.projectStatus;
   }
+  // Get value for Search element
+  OnselectSearch(event)
+  {
+    this.Searchvalue=event;
+    console.log("Event in search "+event)
+  }
+  // Get value for Sorting
+  // Onselect(event)
+  // {
+  //   this.SortValue=event;
+  //   console.log("Event in sort "+event)
+
+  // }
+  // Code For search
+  onKeydownEvent() {
+    var input, filter, table, tr, td, i, txtValue;
+    input = document.getElementById("myInput");
+    filter = input.value.toUpperCase();
+    table = document.getElementById("myTable");
+    tr = table.getElementsByTagName("tr");
+    for (i = 0; i < tr.length; i++) {
+      td =tr[i].getElementsByTagName("td")[this.Searchvalue]  ;
+      if (td) {
+        txtValue = td.textContent || td.innerText;
+        if (txtValue.toUpperCase().indexOf(filter) > -1) {
+          tr[i].style.display = "";
+
+        } else {
+          tr[i].style.display = "none";
+
+        }
+      }
+    }
+  }
+  // Code For Sorting
+  OnselectSortItem(event) {
+    console.log("In search item"+event);
+
+    var table, rows, switching, i, x, y, shouldSwitch;
+    table = document.getElementById("myTable");
+    switching = true;
+    while (switching) {
+      switching = false;
+      rows = table.rows;
+      for (i = 1; i < (rows.length - 1); i++) {
+        shouldSwitch = false;
+        x = rows[i].getElementsByTagName("TD")[event];
+        y = rows[i + 1].getElementsByTagName("TD")[event];
+        if (x.innerHTML.toLowerCase() > y.innerHTML.toLowerCase()) {
+          shouldSwitch = true;
+          break;
+        }
+      }
+      if (shouldSwitch) {
+        rows[i].parentNode.insertBefore(rows[i + 1], rows[i]);
+        switching = true;
+      }
+    }
+  }
+
 }
