4cdd45e97c920101304bd295d52597c7e4d55a92
swati612 <60620512+swati612@users.noreply.github.com>
2020-04-06 14:29:54 +0530

    Defined Role and user APIs, used those apis in particular component to  get, post, delete and update data.

--- src/main/angular/src/app/pages/main/role/role.component.ts
+++ src/main/angular/src/app/pages/main/role/role.component.ts
@@ -1,15 +1,55 @@
+import { AdminService } from './../../../services/admin/admin.service';
 import { Component, OnInit } from '@angular/core';
-
+import { _keyValueDiffersFactory } from '@angular/core/src/application_module';
+import { hasLifecycleHook } from '@angular/compiler/src/lifecycle_reflector';
+import { Router }  from '@angular/router';
 @Component({
   selector: 'app-role',
   templateUrl: './role.component.html',
   styleUrls: ['./role.component.scss']
 })
 export class RoleComponent implements OnInit {
-
-  constructor() { }
+  public roles = [];
+  isloader:boolean=true
+  constructor(private _adminService: AdminService,
+    private route:Router ) { }
 
   ngOnInit() {
-  }
+    this._adminService.getRoles()
+      .subscribe(data => {
+        this.roles = data
+        this.isloader=false
+      },
+        error => {
+          console.log(error);
 
+        })
+  }
+  editrole(item) {
+    this._adminService.selecetedRole.next(item);
+    this.route.navigate(['roles',item.id])
+
+  }
+  deleterole(item) {
+    this._adminService.deleteRole(item.id).subscribe(data => {
+      item.active = false;
+    },
+      error => {
+        console.log(error);
+
+      })
+
+  }
+  activaterole(item)
+  {this._adminService.deleteRole(item.id).subscribe(data => {
+    item.active = true;
+  },
+    error => {
+      console.log(error);
+
+    })
+  }
+  createRole(){
+   this.route.navigate(['roles/create'])
+  }
 }
--- src/main/angular/src/app/pages/main/user/user.component.ts
+++ src/main/angular/src/app/pages/main/user/user.component.ts
@@ -1,4 +1,6 @@
+import { Router } from '@angular/router';
 import { Component, OnInit } from '@angular/core';
+import { AdminService } from './../../../services/admin/admin.service';
 
 @Component({
   selector: 'app-user',
@@ -7,9 +9,45 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UserComponent implements OnInit {
 
-  constructor() { }
-
+  constructor(private _admin: AdminService,
+    private route: Router) { }
+  public users = []
+  isloader:boolean=true
   ngOnInit() {
+    this._admin.getUsers().subscribe
+      (data => {
+        this.users = data
+        this.isloader=false
+      },
+        error => {
+        }
+      )
   }
 
+
+
+  deleteuser(item) {
+    this._admin.deleteUser(item.id).subscribe
+      (data => {
+        item.active = false
+      },
+        error => {
+        })
+  }
+
+  createuser() {
+    this.route.navigate(['users/create'])
+
+  }
+  edituser(item) {
+    this._admin.selecetedUser.next(item);
+    this.route.navigate(['users', item.id])
+  }
+  activateuser(item){
+    this._admin.activateUser(item.id).subscribe(data => {
+      item.active = true
+    },
+      error => {
+      })
+  }
 }
